41f6df25084d6329a5f5452bdfe1207dc4c53c1a
PantiLord <dolina.rmsk@gmail.com>
2026-05-13 10:53:39 +0200

    zdjecia pojazdow

--- src/app/todo/vehicles/vehicles.ts
+++ src/app/todo/vehicles/vehicles.ts
@@ -25,7 +25,7 @@ export class VehiclesComponent {
       cargo_capacity: '1000',
       consumables: 'unknown',
       description: 'AT-AT to ogromny, czteronożny pojazd kroczący używany przez Imperium Galaktyczne. Jest uzbrojony w potężne działa laserowe i jest wykorzystywany głównie do transportu wojsk i wsparcia ogniowego na polu bitwy.',
-      image: 'assets/img/vehicles/at-at.png'
+      image: '/assets/img/vehicles/at-at.png'
     },
     'at-st': {
       name: 'AT-ST',
@@ -40,7 +40,7 @@ export class VehiclesComponent {
       cargo_capacity: '200',
       consumables: 'none',
       description: 'AT-ST to mniejszy, dwunożny pojazd kroczący używany przez Imperium Galaktyczne. Jest uzbrojony w działka laserowe i jest wykorzystywany głównie do patrolowania i wsparcia piechoty na polu bitwy.',
-      image: 'https://starwars-visualguide.com/assets/img/vehicles/7.jpg'
+      image: '/assets/img/vehicles/at-st.png'
     },
     'juggernaut': {
       name: 'Juggernaut',
@@ -55,7 +55,7 @@ export class VehiclesComponent {
       cargo_capacity: 'unknown',
       consumables: 'unknown',
       description: 'Juggernaut to potężny pojazd kroczący używany przez Separatystów podczas Wojen Klonów. Jest uzbrojony w ciężkie działa i jest wykorzystywany głównie do transportu wojsk i wsparcia ogniowego na polu bitwy.',
-      image: 'https://starwars-visualguide.com/assets/img/vehicles/17.jpg'
+      image: '/assets/img/vehicles/juggernaut.png'
     },
     'mtt': {
       name: 'MTT',
@@ -70,7 +70,7 @@ export class VehiclesComponent {
       cargo_capacity: '12000',
       consumables: 'unknown',
       description: 'MTT to duży pojazd transportowy używany przez Separatystów podczas Wojen Klonów. Jest uzbrojony w działka laserowe i jest wykorzystywany głównie do transportu dużej liczby żołnierzy na pole bitwy.',
-      image: 'https://starwars-visualguide.com/assets/img/vehicles/18.jpg'
+      image: '/assets/img/vehicles/mtt.png'
     },
     'sandcrawler': {
       name: 'Sandcrawler',
@@ -85,7 +85,7 @@ export class VehiclesComponent {
       cargo_capacity: '50000',
       consumables: '2 months',
       description: 'Sandcrawler to ogromny pojazd kroczący używany przez Jawów na pustynnej planecie Tatooine. Jest wykorzystywany głównie do transportu i handlu różnymi towarami, a także do zbierania złomu i części z wraków statków kosmicznych.',
-      image: 'https://starwars-visualguide.com/assets/img/vehicles/4.jpg'
+      image: '/assets/img/vehicles/sandcrawler.png'
     },
     'speeder-bike': {
       name: '74-Z Speeder Bike',
@@ -100,12 +100,27 @@ export class VehiclesComponent {
       cargo_capacity: '4',
       consumables: '1 day',
       description: '74-Z Speeder Bike to szybki, jednoosobowy pojazd używany przez Imperium Galaktyczne. Jest wykorzystywany głównie do patrolowania i szybkiego przemieszczania się po różnych terenach, zwłaszcza w lasach Endoru.',
-      image: 'https://starwars-visualguide.com/assets/img/vehicles/30.jpg'
+      image: '/assets/img/vehicles/speeder.png'
+     },
+    '74-z-speeder-bike': {
+      name: '74-Z Speeder Bike',
+      model: '74-Z Speeder Bike',
+      manufacturer: 'Aratech Repulsor Company',
+      vehicle_class: 'speeder',
+      cost_in_credits: '8000',
+      length: '3.68',
+      max_atmosphering_speed: '485',
+      crew: '1',
+      passengers: '1',
+      cargo_capacity: '4',
+      consumables: '1 day',
+      description: '74-Z Speeder Bike to szybki, jednoosobowy pojazd używany przez Imperium Galaktyczne. Jest wykorzystywany głównie do patrolowania i szybkiego przemieszczania się po różnych terenach, zwłaszcza w lasach Endoru.',
+      image: '/assets/img/vehicles/speeder.png'
      }
   };
 
   constructor(private route: ActivatedRoute, private location: Location, private router: Router) {
-    this.vehiclesId = this.route.snapshot.paramMap.get('id')?.toLowerCase() ?? null;
+    this.vehiclesId = this.normalizeId(this.route.snapshot.paramMap.get('id'));
   }
 
   goBack() {
@@ -114,6 +129,16 @@ export class VehiclesComponent {
     } else {
       this.router.navigate(['/']);
     }
+  }
+
+  normalizeId(id: string | null): string | null {
+    return id
+      ? id
+          .trim()
+          .toLowerCase()
+          .replace(/[^a-z0-9]+/g, '-')
+          .replace(/^-+|-+$/g, '')
+      : null;
   }
 
   get vehicles() {
