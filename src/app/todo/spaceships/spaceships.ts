a8ef8d4f214d205340e8129b7b5f1ed0777e34ab
Adam4747 <adamgeda@wp.pl>
2026-05-14 09:42:25 +0200

    spaceships

--- src/app/todo/spaceships/spaceships.ts
+++ src/app/todo/spaceships/spaceships.ts
@@ -20,7 +20,8 @@ export class SpaceshipsComponent {
       crew: '3',
       passengers: '2',
       length: '14.5',
-      description: 'ARC-170 to trzymiejscowy myśliwiec szturmowy używany przez Republikę Galaktyczną podczas wojen klonów. Łączy w sobie rolę myśliwca, bombowca i małego transportu, dzięki czemu jest wszechstronny na polu bitwy.',
+      description: 'ARC-170 to ciężki myśliwiec Republiki, używany głównie przez pilotów klonów podczas wojen klonów. Maszyna wyróżnia się trzema miejscami w kokpicie, składanymi skrzydłami typu S-foils oraz dużym zasięgiem. Uzbrojona w lasery i torpedy protonowe, była skuteczna w walce z myśliwcami Separatystów. Jej solidna konstrukcja zapewniała wytrzymałość, ale kosztem zwrotności w starciach kosmicznych. Często współpracowała z innymi jednostkami, pełniąc rolę wsparcia i patrolu w trudnych misjach bojowych różnorodnych.',
+      image: '/assets/img/spaceships/arc-170.png',
     },
     'devastator': {
       name: 'Devastator',
@@ -31,7 +32,8 @@ export class SpaceshipsComponent {
       crew: '47060',
       passengers: '0',
       length: '1600',
-      description: 'Devastator to potężny niszczyciel gwiezdny Imperium, o masywnych możliwościach bojowych i wielkiej obecności nad systemami planetarnymi.',
+      description: 'Devastator to potężny Gwiezdny Niszczyciel Imperium, dowodzony przez Dartha Vadera. Okręt klasy Imperial I słynie z ogromnej siły ognia, zdolności projekcji mocy oraz zastraszającej obecności. Uczestniczył w pościgu za Tantive IV na początku Nowej Nadziei. Wyposażony w liczne baterie turbolaserów i hangary, pełnił rolę flagowego symbolu dominacji Imperium oraz kontroli nad galaktyką. Jego załoga liczyła tysiące żołnierzy, a systemy sensorów umożliwiały wykrywanie wrogów z dużych odległości.',
+      image: '/assets/img/spaceships/devastator.png',
     },
     'death-star': {
       name: 'Gwiazda śmierci',
@@ -42,7 +44,8 @@ export class SpaceshipsComponent {
       crew: '342953',
       passengers: '843342',
       length: '120000',
-      description: 'Gwiazda śmierci to stacja bojowa zdolna do zniszczenia całej planety jednym strzałem. To najbardziej przerażająca superbroń Imperium.',
+      description: 'Gwiazda Śmierci to ogromna stacja bojowa Imperium, zdolna do niszczenia całych planet jednym strzałem. Jej budowa była tajnym projektem, nadzorowanym przez Dartha Vadera i Imperatora. Uzbrojona w superlaser oraz liczne działa, stanowiła symbol absolutnej władzy i terroru. Mimo potęgi miała słaby punkt, który Rebelianci wykorzystali do jej zniszczenia w bitwie pod Yavin. Jej rozmiary pozwalały pomieścić tysiące żołnierzy oraz statków bojowych wewnątrz.',
+      image: '/assets/img/spaceships/death-star.png',
     },
     'millennium-falcon': {
       name: 'Sokół Millennium',
@@ -53,7 +56,8 @@ export class SpaceshipsComponent {
       crew: '2',
       passengers: '6',
       length: '34.75',
-      description: 'Sokół Millennium to szybki i zwrotny frachtowiec z najlepszym w galaktyce hypernapędem. Idealny do przemytniczych misji i ucieczek przed Imperium.',
+      description: 'Sokół Millennium to legendarny frachtowiec, pilotowany przez Han Solo i Chewbacca. Choć wygląda na zniszczony, jest niezwykle szybki dzięki zmodyfikowanemu napędowi nadświetlnemu. Brał udział w wielu kluczowych bitwach, wspierając Rebelię przeciw Imperium. Uzbrojony w działka laserowe i osłony, potrafił uciec nawet z najtrudniejszych sytuacji. Jego charakterystyczny kształt i historia czynią go jednym z najbardziej rozpoznawalnych statków w galaktyce. Często ukrywał się przed wrogami dzięki sprytowi załogi.',
+      image: '/assets/img/spaceships/millennium-falcon.png',
     },
     'tie-whisper': {
       name: 'TIE Whisper',
@@ -64,7 +68,8 @@ export class SpaceshipsComponent {
       crew: '1',
       passengers: '0',
       length: '9.7',
-      description: 'TIE Whisper to szybki przechwytujący myśliwiec Imperium o wysokiej zwrotności i agresywnym profilu bojowym.',
+      description: 'TIE Whisper to zaawansowany myśliwiec, używany przez Najwyższy Porządek. Był osobistą maszyną Kylo Ren, zaprojektowaną z myślą o szybkości i skrytości. Wyposażony w nowoczesne systemy naprowadzania oraz potężne działka laserowe, przewyższał standardowe myśliwce TIE. Jego ciemny wygląd i ulepszone osłony czyniły go groźnym przeciwnikiem w walce kosmicznej. TIE Whisper symbolizował rosnącą potęgę i technologiczną przewagę Najwyższego Porządku.',
+      image: '/assets/img/spaceships/tie-whisper.png',
     },
     'x-wing': {
       name: 'X-wing',
@@ -75,7 +80,8 @@ export class SpaceshipsComponent {
       crew: '1',
       passengers: '0',
       length: '12.5',
-      description: 'X-wing to legendarny myśliwiec Rebelii, łączący szybkość, zwrotność i siłę ognia. Sprawdza się zarówno w dogfightach, jak i w atakach na większe cele.',
+      description: 'X-wing to wszechstronny myśliwiec Rebelii, znany z charakterystycznych skrzydeł w układzie X. Używany przez pilotów takich jak Luke Skywalker, odegrał kluczową rolę w walce przeciw Imperium. Wyposażony w lasery i torpedy protonowe, łączył siłę ognia z dobrą zwrotnością. Dzięki hipernapędowi mógł działać samodzielnie na dużych dystansach. Stał się symbolem nadziei oraz odwagi Rebelii w walce o wolność galaktyki.',
+      image: '/assets/img/spaceships/x-wing.png',
     }
   };
 
