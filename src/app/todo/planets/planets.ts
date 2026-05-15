d1066e0530152451838aa179d13488fde31d0509
Adam4747 <adamgeda@wp.pl>
2026-05-15 10:35:11 +0200

    swapi w planetach, opis w postaciach

--- src/app/todo/planets/planets.ts
+++ src/app/todo/planets/planets.ts
@@ -1,53 +1,70 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-planets',
-  styleUrls: ['./planets.css'],
-  templateUrl: './planets.html', 
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './planets.html',
+  styleUrl: './planets.css',
 })
 export class PlanetsComponent {
 
-  planetId: string | null = null;
+  planet: any;
+  loading = true; // 👈 DODANE
+  apiId: number | null = null;
+  routeId: string | null = null;
 
-  planets: any = {
-    'alderaan': {
-      name: 'Alderaan',
-      description: 'Planeta zniszczona przez Gwiazdę Śmierci',
-      image: '/assets/img/planets/alderaan.png',
-    },
-    'coruscant': {
-      name: 'Coruscant',
-      description: 'Stolica galaktyki, planeta-miasto',
-      image: '/assets/img/planets/coruscant.png',
-    },
-    'dagobah': {
-      name: 'Dagobah',
-      description: 'Bagienna planeta, miejsce ukrycia Yody',
-      image: '/assets/img/planets/dagobah.png',
-    },
-    'hoth': {
-      name: 'Hoth',     
-      description: 'Planeta zimna, miejsce operacji Clone',
-      image: '/assets/img/planets/hoth.png',
-    },
-    'naboo': {
-      name: 'Naboo',
-      description: 'Planeta o bujnej przyrodzie, miejsce pochodzenia Padmé Amidali',
-      image: '/assets/img/planets/naboo.png',
-    },
-    'tatooine': {
-      name: 'Tatooine',
-      description: 'Pustynna planeta, miejsce urodzenia Lukea Skywalkera',
-      image: '/assets/img/planets/tatooine.png',
-    } 
+  descriptions: Record<string, string> = {
+  'alderaan': 'Alderaan to spokojna planeta znana z wysokiego poziomu kultury i dyplomacji. Została zniszczona przez Gwiazdę Śmierci.',
+  'coruscant': 'Coruscant to planetarne miasto będące centrum politycznym galaktyki i siedzibą Senatu Republiki oraz Zakonu Jedi.',
+  'dagobah': 'Dagobah to dzika, bagienna planeta pełna gęstej roślinności, znana jako miejsce wygnania Yody.',
+  'hutt': 'Hutt to planeta zamieszkiwana przez przestępcze syndykaty Huttów, znana z nielegalnych interesów i mafijnej kontroli.',
+  'naboo': 'Naboo to malownicza planeta o bujnej naturze i rozwiniętej cywilizacji, znana z Pałacu królewskiego w Theed.',
+  'tatooine': 'Tatooine to pustynna planeta na obrzeżach galaktyki, znana z podwójnych słońc i ciężkich warunków życia.'
+};
+
+  images: Record<string, string> = {
+    'alderaan': '/assets/img/planets/alderaan.png',
+    'coruscant': '/assets/img/planets/coruscant.png',
+    'dagobah': '/assets/img/planets/dagobah.png',
+    'hutt': '/assets/img/planets/hutt.png',
+    'naboo': '/assets/img/planets/naboo.png',
+    'tatooine': '/assets/img/planets/tatooine.png'
   };
 
-  constructor(private route: ActivatedRoute) {
-    this.planetId = this.route.snapshot.paramMap.get('id');
-  }
+  constructor(
+    private http: HttpClient,
+    private route: ActivatedRoute,
+    private cdr: ChangeDetectorRef
+  ) {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.routeId = id;
+    console.log(this.loading, "1");
+    const map: any = {
+      'alderaan': 2,
+      'coruscant': 9,
+      'dagobah': 5,
+      'hutt': 4,
+      'naboo': 8,
+      'tatooine': 1
+    };
 
-  get planet() {
-    return this.planets[this.planetId || ''];
+    this.apiId = map[id!];
+    console.log(this.loading, "2");
+    this.http.get(`https://swapi.info/api/planets/${this.apiId}`)
+      .subscribe(data => {
+        this.planet = data;
+        console.log("test", data);
+        console.log(this.loading, "3a");
+        this.loading = false; // 👈 DODANE (TU KOŃCZY SIĘ ŁADOWANIE)
+        this.cdr.detectChanges();
+        console.log(this.loading, "3b");
+      });
+
+      console.log(this.loading, "4");
   }
 }
